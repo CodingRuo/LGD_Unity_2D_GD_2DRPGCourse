2bbd4f320eccdd2f470dd224af67ee015f809eb9
René Schober <r.schober@outlook.com>
2024-04-10 20:26:22 +0200

    Adding, Sprites and Finished Blend Tree Animations

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private float speed;
+
+    private readonly int moveX = Animator.StringToHash( "MoveX" );
+    private readonly int moveY = Animator.StringToHash( "MoveY" );
+
+    private PlayerActions   actions;
+    private Rigidbody2D     rb2D;
+    private Animator        animator;
+    private Vector2         moveDirection;
+
+    private void Awake() {
+        actions  = new PlayerActions();
+        animator = GetComponent<Animator>();
+        rb2D     = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        ReadMovement();
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+    private void Move() {
+        rb2D.MovePosition( rb2D.position + moveDirection * ( speed * Time.fixedDeltaTime ) );
+    }
+
+    private void ReadMovement() {
+
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+
+        if ( moveDirection == Vector2.zero ) return;
+
+        animator.SetFloat( moveX, moveDirection.x );
+        animator.SetFloat( moveY, moveDirection.y );
+
+    }
+
+    private void OnEnable() {
+        actions.Enable();
+    }
+
+    private void OnDisable() {
+        actions.Disable();
+    }
+}
